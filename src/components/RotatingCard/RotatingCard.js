356c7540d3e17eff22b3ca3fed936bf453ff6168
elamahpla <theamazingness@gmail.com>
2018-08-03 11:18:56 -0500

    _rotatingcard.scss.

---
+++ src/components/RotatingCard/RotatingCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RotatingCard = () => (
+  <div className="RotatingCard">
+    <div className="RotatingCard-Side">
+      <div className="RotatingCard-Side-Front">
+      </div>
+      <div className="RotatingCard-Picture RotatingCard-Picture-1">
+        &nbsp;
+      </div>
+      <div className="RotatingCard-Side RotatingCard-Side-Back RotatingCard-Side-Back-1">
+      </div>  
+    </div>
+  </div>
+);
+
+export default RotatingCard;
